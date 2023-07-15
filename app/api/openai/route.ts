bc9dcf560f6376f699db52552669333e82858c7d
timwang <61922079@qq.com>
2023-07-15 12:40:02 +0800

    add openai calling

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,9 +1,22 @@
-import { cookies } from 'next/headers';
-
-export const runtime = 'edge';
-
-export async function GET(request: Request) {
-	return new Response('Test open AI proxy', {
-		status: 200,
-	});
+import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+ 
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(config)
+ 
+export const runtime = 'edge'
+ 
+export async function POST(req: Request) {
+  const { prompt } = await req.json()
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    stream: true,
+    temperature: 0.6,
+    prompt: prompt,
+  })
+ 
+  const stream = OpenAIStream(response)
+  return new StreamingTextResponse(stream)
 }
