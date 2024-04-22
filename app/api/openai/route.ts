3f13d8959b46caf69a2cf83266426a6156dcfbf1
lazyload0505 <wang.shuai.0505@gmail.com>
2024-04-22 10:56:48 +0800

    Update the openai lib and configuration.

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,37 +1,35 @@
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAI } from 'openai'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
  
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-const openai = new OpenAIApi(config)
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+	baseURL: process.env.OPENAI_API_BASE_URL
+});
  
 export const runtime = 'edge'
  
 export async function POST(req: Request) {
   const { msgs } = await req.json();
-  const completion = await openai.createChatCompletion({
-	model: "gpt-3.5-turbo-16k",
+
+  const completion = await await openai.chat.completions.create({
+	model: "gpt-3.5-turbo",
+	// https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
+	// gpt-3.5-turbo-1106
+	// gpt-3.5-turbo-instruct
+	// gpt-3.5-turbo
 	// gpt-3.5-turbo-16k
+	// gpt-4
 	// gpt-4-32k
+	// gpt-4-1106-preview
 	messages: [
 	  { role: "system", content: "You are a blockchain developer and architect expert. Generate every answer with less than 800 characters." },
 	  ...msgs
 	],
-	max_tokens: 8000,
+	max_tokens: 4000,
 	temperature: 0,
 	stream: true,
   });
 
   const stream = OpenAIStream(completion);
   return new StreamingTextResponse(stream);
- 
-//   return new Response(response.body, {
-// 	headers: {
-// 	  "Access-Control-Allow-Origin": "*",
-// 	  "Content-Type": "text/event-stream;charset=utf-8",
-// 	  "Cache-Control": "no-cache, no-transform",
-// 	  "X-Accel-Buffering": "no",
-// 	},
-//   })
 }
